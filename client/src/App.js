8f03a02b5750dc068901d06c8bf3d66e2286160b
Nurul Aziz <nurulaziz.nz@gmail.com>
2021-06-11 15:27:44 +0600

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import FileUpload from './components/FileUpload';
 
 const App = () => (
   <div className='container mt-4'>
-    <h4 className='display-4 text-center mb-4'> File Upload</h4>
+    <h4 className='display-4 text-center mb-4'>File Upload</h4>
 
     <FileUpload />
   </div>
--- server/app.js
+++ server/app.js
@@ -1,29 +1,119 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const crypto = require('crypto');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
 const Grid = require('gridfs-stream');
+const databaseURI = require('./config');
 
-const dbURI = require('./config');
-const routes = require('./routes/routes');
-
-// Initalize app
 const app = express();
-const port = 5000;
 
 // Middleware
 app.use(bodyParser.json());
 
-// Routes
-app.use(routes);
-
 // Create mongo connection
-const connection = mongoose.createConnection(dbURI);
-connection.once('open', () =>
-{
-  // Init stream
-  let gridfs = Grid(connection.db, mongoose.mongo);
-  gridfs.collection('uploads');
-  app.listen(port, () => console.log(`Server started on port ${port}`));
+const connection = mongoose.connect(databaseURI,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex:true,
+    useFindAndModify: false
+  })
+  .then((_res) =>
+  {
+    // let gridfs = Grid(connection.db, mongoose.mongo);
+    // gridfs.collection('uploads');
+    const port = 5000;
+    app.listen(port, () => console.log(`Server started on port ${port}`));
+  })
+  .catch((err) => console.log(err));
+
+// Initialize storage engine
+const storage = new GridFsStorage({
+  url: databaseURI,
+  file: (_req, file) =>
+  {
+    return new Promise((resolve, reject) =>
+    {
+      crypto.randomBytes(16, (err, buf) =>
+      {
+        if (err)
+          return reject(err);
+
+        const filename = buf.toString('hex') + path.extname(file.filename);
+        
+        const fileInfo =
+        {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  },
+  options: {
+    useUnifiedTopology: true,
+  }
 });
 
-module.exports = gridfs;
+//Access the storage engine using multer to upload files
+const upload = multer({ storage });
+
+//Initial page (loads form and uploaded files)
+app.get('/', (req, res) =>
+{
+  gridfs.files.find().toArray((err, files) =>
+  {
+    if (!files || files.length === 0)
+      res.render('index', { files: false });
+    else
+    {
+      files.map(file =>
+        {
+          if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') file.isImage = true;
+          else file.isImage = false;
+        });
+      return res.json({ files: files });
+    }
+  });
+});
+
+//Uploads file to DB
+app.post('/upload', upload.single('file'), (req, res) =>
+{
+  res.redirect('/');
+});
+
+//Display all files in JSON
+app.get('/files', (_req, res) =>
+{
+  gridfs.files.find().toArray((_err, files) =>
+  {
+    if (!files || files.length === 0)
+      return res.status(404).json({err: 'No files exist'});
+    return res.json(files);
+  });
+});
+
+//Display single file object from DB
+app.get('/files/:filename', (req, res) =>
+{
+  gridfs.files.findOne({ filename: req.params.filename }, (err, file) =>
+  {
+    if (!file || file.length === 0)
+      return res.status(404).json({err: 'No file exists'});
+    return res.json(file);
+  });
+});
+
+//Delete file from DB
+app.delete('/files/:id', (req, res) =>
+{
+  gridfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) =>
+  {
+    if (err) return res.status(404).json({ err: err });
+    res.redirect('/');
+  });
+});
